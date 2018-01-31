65f3985a302cb40c5f47ee166fdd34e5146a0386
Isaac <31518729+Ikegorgon@users.noreply.github.com>
2018-01-31 12:57:23 -0700

    Added Runner

---
+++ DataStructures/Runner.cpp
@@ -0,0 +1,14 @@
+//
+//  Runner.cpp
+//  DataStructures
+//
+//  Created by Lamb, Isaac on 1/31/18.
+//  Copyright © 2018 Lamb, Isaac. All rights reserved.
+//
+
+#include "Controller/Controller.hpp"
+int main() {
+    Controller app;
+    app.start();
+    return 0;
+}
