6a0552e3150a39e1ccad37336eb36fcc45415998
Isaac <31518729+Ikegorgon@users.noreply.github.com>
2018-03-27 13:04:33 -0600

    Added BinaryTreeTester

---
+++ DataStructures/Testers/BinaryTreeTester.cpp
@@ -0,0 +1,13 @@
+//
+//  BinaryTreeTester.cpp
+//  DataStructures
+//
+//  Created by Lamb, Isaac on 3/27/18.
+//  Copyright © 2018 Lamb, Isaac. All rights reserved.
+//
+
+#include "BinaryTreeTester.hpp"
+
+void BinaryTreeTester :: testBinaryTree() {
+    
+}
---
+++ DataStructures/Testers/BinaryTreeTester.hpp
@@ -0,0 +1,22 @@
+//
+//  BinaryTreeTester.hpp
+//  DataStructures
+//
+//  Created by Lamb, Isaac on 3/27/18.
+//  Copyright © 2018 Lamb, Isaac. All rights reserved.
+//
+
+#ifndef BinaryTreeTester_hpp
+#define BinaryTreeTester_hpp
+
+#include <iostream>
+#include "../Model/Nodes/BinaryTreeNode.hpp"
+
+class BinaryTreeTester {
+private:
+    
+public:
+    void testBinaryTree();
+};
+
+#endif /* BinaryTreeTester_hpp */
